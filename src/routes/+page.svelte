36e2620f85535d038f15ca4b01830fc226a0b4ed
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-03-20 15:39:13 +0100

    feat: remove ticker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,21 +2,15 @@
 	import { createMapStore, MAPSTORE_CONTEXT_KEY } from '$lib/stores';
 	import { setContext } from 'svelte';
 	import EmergencyCall from '../components/EmergencyCall.svelte';
+	import InfoBox from '../components/InfoBox.svelte';
 	import Map from '../components/Map.svelte';
 
 	const mapStore = createMapStore();
 	setContext(MAPSTORE_CONTEXT_KEY, mapStore);
-	import InfoBox from '../components/InfoBox.svelte';
-	import SpeechBubble from '../components/SpeechBubble.svelte';
-	import Ticker from '../components/Ticker.svelte';
 </script>
 
 <div class="page-root mx-auto max-w-[800px] p-4">
 	<h1>Kiezbox Notfallapp</h1>
-
-	<h2>Aktuelle News</h2>
-	<Ticker />
-
 	<h1>EmergencyCall</h1>
 	<EmergencyCall />
 	<InfoBox
@@ -24,9 +18,8 @@
 		textBodyOrList={'Wir empfehlen einen Vorrat an Lebensmitteln für mind. 3 Tage pro Person. Des weiteren sollten Sie mind. 1,5l Trinkwasser und 0,5l Wasser pro Tag zum Kochen und für Hygiene einplanen.'}
 		links={[{ href: 'Vorratskalkulator des Bundes', text: 'Vorratskalkulator des Bundes' }]}
 	></InfoBox>
-	
-	<Map />
 
+	<Map />
 </div>
 
 <style></style>
