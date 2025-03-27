8a6937e6fbbf2b7c6fb0cb431fb5b0fd868d0371
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-03-27 15:32:39 +0100

    feat: separate state

---
+++ src/lib/state/layerState.svelte.ts
@@ -0,0 +1,35 @@
+import { LAYER_CONFIG } from '$lib/config/layers';
+import { mapState } from './mapState.svelte';
+
+function createLayerState() {
+  const layerVisibility = $state<Record<string, string>>({});
+
+  LAYER_CONFIG.forEach(layer => {
+    layerVisibility[layer.id] = layer.layout?.visibility ?? 'visible';
+  });
+
+  return {
+    layerVisibility,
+    toggleLayerVisibility(layerId: string) {
+
+      if (!mapState.map) return;
+
+      const currentVisibility = layerVisibility[layerId];
+
+      const newVisibility = currentVisibility === 'visible' ? 'none' : 'visible';
+
+      mapState.map.setLayoutProperty(layerId, 'visibility', newVisibility);
+      
+      layerVisibility[layerId] = newVisibility;
+    },
+    setLayerVisibility(layerId: string, visibility: 'visible' | 'none') {
+      if (!mapState.map) return;
+
+      mapState.map.setLayoutProperty(layerId, 'visibility', visibility);
+      
+      layerVisibility[layerId] = visibility;
+    }
+  };
+}
+
+export const layerState = createLayerState();
--- src/lib/state/state.svelte.ts
+++ src/lib/state/state.svelte.ts
@@ -1,25 +1,3 @@
-interface MapState {
-  map:  null | maplibregl.Map; 
-  layers: string[];
-  loaded: boolean;
-}
-
-// todo: fix any type
-interface PoiState {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  properties: any;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  layer: any;
-}
-
-export const mapState = $state<MapState >({
-  map: null,
-  layers: [],
-  loaded: false
-});
-
-export const poiState = $state<PoiState>({
-  properties: null,
-  layer: null
-});
-
+export * from '$lib/state/layerState.svelte';
+export * from '$lib/state/poiState.svelte';
+export * from '$lib/state/mapState.svelte';
