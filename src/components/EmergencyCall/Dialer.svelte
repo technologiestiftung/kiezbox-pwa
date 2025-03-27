20ca9ca902bb248e4c86a93787ff951a8a59ea60
Jonathan <jonathan.haudenschild@ts.berlin>
2025-03-27 18:42:47 +0100

    fix: sticky button

--- src/components/EmergencyCall/Dialer.svelte
+++ src/components/EmergencyCall/Dialer.svelte
@@ -6,9 +6,9 @@
 </script>
 
 <div
-	class={`Dialer-root grid max-w-[43.75rem] justify-center ${isEmergency ? 'bg-notruf-light' : 'bg-purple-light'}`}
+	class={`Dialer-root relative grid max-w-[43.75rem] justify-center ${isEmergency ? 'bg-notruf-light' : 'bg-purple-light'}`}
 >
-	<div class=" relative flex justify-center p-6 md:w-[29rem]">
+	<div class=" flex justify-center p-6 md:w-[29rem]">
 		<div class="notruf-container flex flex-col items-start">
 			<span class="body-large-bold">
 				{$t('content.emergency_phone.title')}
@@ -18,10 +18,12 @@
 			</span>
 		</div>
 	</div>
+	<div
+		class={`absolute -bottom-14 z-10 flex h-14 w-full justify-center ${isEmergency ? 'bg-notruf-light' : 'bg-purple-light'}`}
+	></div>
 </div>
-<div
-	class={`sticky top-0 z-10 flex justify-center ${isEmergency ? 'bg-notruf-light' : 'bg-purple-light'}`}
->
+
+<div class={`sticky top-0 z-10 flex justify-center`}>
 	<div class="w-full px-6 md:w-[29rem]">
 		<EmergencyCallButton
 			isActive={isEmergency}
