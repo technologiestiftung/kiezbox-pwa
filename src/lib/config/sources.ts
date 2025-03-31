d6cadc10bc27c3788852292bb79fe60e244280c1
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-03-31 15:18:07 +0200

    feat: update sources

--- src/lib/config/sources.ts
+++ src/lib/config/sources.ts
@@ -6,44 +6,54 @@ import waterPumpsData from '$lib/data/water-pumps.json';
 import type { GeoJSON } from 'geojson';
 
 export interface SourceConfig {
-  id: string;
-  type: 'raster' | 'geojson';
-  tiles?: string[];
-  tileSize?: number;
-  attribution?: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  data?: any;
+	id: string;
+	type: 'raster' | 'geojson' | 'vector';
+	tiles?: string[];
+	tileSize?: number;
+	attribution?: string;
+	minzoom?: number;
+	maxzoom?: number;
+	data?: GeoJSON;
 }
 
 export const BASE_SOURCES_CONFIG: SourceConfig[] = [
-  {
-    id: 'osmLocal',
-    type: 'raster',
-    tiles: ['http://localhost:3000/tiles/{z}/{x}/{y}.png'],
-    tileSize: 256,
-    attribution: '© OpenStreetMap contributors'
-  },
+	{
+		id: 'osm',
+		type: 'raster',
+		tiles: ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
+		tileSize: 256,
+		attribution: '© OpenStreetMap contributors',
+		maxzoom: 18
+	},
+	{
+		id: 'osmLocal',
+		type: 'raster',
+		tiles: ['/tiles/{z}/{x}/{y}.png'],
+		tileSize: 256,
+		attribution: '© OpenStreetMap contributors',
+		maxzoom: 15
+	}
 ];
 
 export const SOURCES_CONFIG: SourceConfig[] = [
-  {
-    id: 'drinkingWater',
-    type: 'geojson',
-    data: drinkingWaterData as unknown as GeoJSON
-  },
-  {
-    id: 'toilets',
-    type: 'geojson',
-    data: toiletsData as unknown as GeoJSON
-  },
-  {
-    id: 'waterPumps',
-    type: 'geojson',
-    data: waterPumpsData as unknown as GeoJSON
-  },
-  {
-    id: 'defies',
-    type: 'geojson',
-    data: defiesData as unknown as GeoJSON
-  }
+	{
+		id: 'drinkingWater',
+		type: 'geojson',
+		data: drinkingWaterData as unknown as GeoJSON
+	},
+	{
+		id: 'toilets',
+		type: 'geojson',
+		data: toiletsData as unknown as GeoJSON
+	},
+	{
+		id: 'waterPumps',
+		type: 'geojson',
+		data: waterPumpsData as unknown as GeoJSON
+	},
+	{
+		id: 'defies',
+		type: 'geojson',
+		data: defiesData as unknown as GeoJSON
+	}
 ];
